2b8e3800b97688264957a837e60ab9bd46ad14cf
aligomaa56 <3looosh56565656@gmail.com>
2024-11-08 14:28:29 +0200

    Upload js file

---
+++ script.js
@@ -0,0 +1,7 @@
+// Navigation Toggle
+const navToggle = document.getElementById('navToggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
